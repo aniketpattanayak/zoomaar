2b7fee7bd49c0e9bb98649c72a94d10e1f49e121
Aniket Pattanayak <82846434+aniketpattanayak@users.noreply.github.com>
2025-03-19 11:43:34 +0530

    Update App.jsx

--- seller/src/App.jsx
+++ seller/src/App.jsx
@@ -10,7 +10,7 @@ import LiveStream from './components/LiveStream';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-export const backendUrl = import.meta.env.VITE_BACKEND_URL;
+export const backendUrl = "https://zoomaar.onrender.com";
 export const currency = '$';
 
 const App = () => {
